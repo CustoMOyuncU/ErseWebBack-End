90c2d265b96ed442112b88beda08761c7908f327
CustoMOyuncU <kraloyuntv2@gmail.com>
2021-08-30 13:43:46 +0300

    Data Access Operations

---
+++ DataAccess/Concrete/EntityFramework/ClothesContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class ClothesContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;DataBase=ErseWeb;Trusted_Connection=true");
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Clothe> Clothes { get; set; }
+        public DbSet<ClotheImage> ClotheImages { get; set; }
+
+    }
+}
