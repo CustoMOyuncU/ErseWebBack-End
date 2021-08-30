d8bf281b02303aff0ce3ff0b49a7a875bec22ecf
CustoMOyuncU <kraloyuntv2@gmail.com>
2021-08-30 13:42:37 +0300

    Dependency

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -17,8 +17,8 @@ namespace Business.DependencyResolvers.Autofac
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
-            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
 
             builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
             builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
@@ -26,22 +26,16 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<ColorManager>().As<IColorService>().SingleInstance();
             builder.RegisterType<EfColorDal>().As<IColorDal>().SingleInstance();
 
-            builder.RegisterType<RentalManager>().As<IRentalService>().SingleInstance();
-            builder.RegisterType<EfRentalDal>().As<IRentalDal>().SingleInstance();
+            builder.RegisterType<ClotheManager>().As<IClotheService>().SingleInstance();
+            builder.RegisterType<EfClotheDal>().As<IClotheDal>().SingleInstance();
 
-            builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
-            builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
+            //builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
+            //builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
 
-            builder.RegisterType<CustomerManager>().As<ICustomerService>().SingleInstance();
-            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>().SingleInstance();
+            builder.RegisterType<ClotheImageManager>().As<IClotheImageService>().SingleInstance();
+            builder.RegisterType<EfClotheImageDal>().As<IClotheImageDal>().SingleInstance();
 
-            builder.RegisterType<CarImageManger>().As<ICarImageService>().SingleInstance();
-            builder.RegisterType<EfCarImageDal>().As<ICarImageDal>().SingleInstance();
-
-            builder.RegisterType<CreditManager>().As<ICreditService>().SingleInstance();
-            builder.RegisterType<EfCreditDal>().As<ICreditDal>().SingleInstance();
-
-            builder.RegisterType<AuthManager>().As<IAuthService>().SingleInstance();
+            //builder.RegisterType<AuthManager>().As<IAuthService>().SingleInstance();
             builder.RegisterType<JwtHelper>().As<ITokenHelper>().SingleInstance();
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
